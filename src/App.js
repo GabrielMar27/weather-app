ee77fcd45a0e908220e354eb978f63cd7350669b
Gabi <marin.gabriel.ionut27@gmail.com>
2023-10-31 23:03:10 +0200

    added deleating data and styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { useState } from "react";
+//components
 import Weather from "./components/api";
 import SearchBar from "./components/form";
 import WeatherCard from "./components/renderData";
 import HoldData from "./components/storedData";
+//styles
+import "./style/general-style.css";
+//mUI styles
+
+import Grid from "@mui/material/Grid";
+import Item from "./components/item";
+import { List, Paper } from "@mui/material";
 
 const App = () => {
   const [weatherData, setWeatherData] = useState(null);
 
   const handleSubmit = async (city) => {
     const result = await Weather(city);
-    console.log(localStorage);
-
     setWeatherData(result);
   };
 
   return (
     <div>
       <h1>Weather Forecast App</h1>
+
       <SearchBar onSubmit={handleSubmit} />
-      <WeatherCard weatherData={weatherData}  />
-      <HoldData weatherData={weatherData}  />
+      <Grid
+        container
+        spacing={3}
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+      >
+        <Grid item xs={6}>
+          <Item>
+            <WeatherCard weatherData={weatherData} />
+          </Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>
+            <Paper style={{ maxHeight: 307, overflow: "auto" }}>
+              <List>
+                {" "}
+                <HoldData weatherData={weatherData} />
+              </List>
+            </Paper>
+          </Item>
+        </Grid>
+      </Grid>
     </div>
   );
 };
--- src/components/storedData.js
+++ src/components/storedData.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { Button, Card } from "@mui/material";
 
 const HoldData = (props) => {
   const storagedData = JSON.parse(localStorage.getItem("searchHistory"));
   const [data, setData] = useState(storagedData || []);
 
+  const deleteData = (index) => {
+    const updatedData = data.filter((_, i) => i !== index);
+    setData(updatedData);
+    localStorage.setItem("searchHistory", JSON.stringify(updatedData));
+    console.log(`deleted ${index}`);
+  };
   useEffect(() => {
     if (props.weatherData) {
       const isDuplicate = data.some(
@@ -23,7 +30,7 @@ const HoldData = (props) => {
   return (
     <>
       {data.map((weatherData, index) => (
-        <div key={index}>
+        <Card variant="outlined" key={index}>
           <h2>{weatherData.name}</h2>
           <p>Temperatură: {weatherData.main.temp} °C</p>
           <p>Descriere: {weatherData.weather[0].description}</p>
@@ -31,7 +38,16 @@ const HoldData = (props) => {
           <p>Umiditate: {weatherData.main.humidity}%</p>
           <p>Presiune: {weatherData.main.pressure}</p>
           <p>Viteză vânt: {weatherData.wind.speed} m/s</p>
-        </div>
+          <Button
+            variant="contained"
+            onClick={() => {
+              if (window.confirm("Sigur doresti sa stergi acesta cautare?"))
+                deleteData(index);
+            }}
+          >
+            Delete
+          </Button>
+        </Card>
       ))}
     </>
   );
